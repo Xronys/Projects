42b3025594b504639b2532e26a3888aab6537262
Igor Kocachev <91168912+Xronys@users.noreply.github.com>
2023-03-24 06:25:36 +0300

    Delete Spring/Twitter directory

--- Spring/Twitter/src/main/java/com/example/twitter/repos/MessageRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.twitter.repos;
-
-import com.example.twitter.domain.Message;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MessageRepo extends CrudRepository<Message,Long> {
-}
